fa8ec08935df7815bec0edf89238107365c0f321
gaetanana <gaetan.gonfiantini@gmail.com>
2023-10-30 13:51:54 +0100

    Modification de la fonction d'authentification

--- app/model/LDAPS/methodeLDAPS.php
+++ app/model/LDAPS/methodeLDAPS.php
@@ -9,35 +9,28 @@ require "connectionLDAPS.php";
 function authentificationAuLDAP($username, $password){
     $ldap_conn = connectionLDAPS::getInstance()->getConnection();
     if ($ldap_conn) {
-        // Connectez-vous d'abord avec un compte de service/administrateur
         if (@ldap_bind($ldap_conn, "cn=admin,dc=mondomaine,dc=local", "adminpassword")) {
-            // Chercher le DN de l'utilisateur en utilisant l'attribut uid
             $search = ldap_search($ldap_conn, "dc=mondomaine,dc=local", "(uid=" . $username . ")");
             if ($search) {
                 $entries = ldap_get_entries($ldap_conn, $search);
                 if ($entries["count"] > 0) {
                     $userdn = $entries[0]["dn"];
-                    // Connexion LDAP avec le DN complet et le mot de passe de l'utilisateur
                     if (@ldap_bind($ldap_conn, $userdn, $password)) {
-                        return true;
+                        return $entries[0]; // Retourne les détails de l'utilisateur
                     } else {
                         return false;
                     }
                 } else {
-                    return false; // L'utilisateur n'a pas été trouvé
+                    return false;
                 }
             } else {
-                return false; // La recherche LDAP a échoué
+                return false;
             }
         } else {
-            return false; // Connexion avec le compte admin échouée
+            return false;
         }
     } else {
-        return false; // Connexion LDAP échouée
+        return false;
     }
 }
-
-
-
-
 ?>
