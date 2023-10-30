ea1a6f4ffbe9c5cf05142e5a312fb5e5d9a887ac
gaetanana <gaetan.gonfiantini@gmail.com>
2023-10-30 14:45:34 +0100

    Ajout de la méthode isAdmin

--- app/model/LDAPS/methodeLDAPS.php
+++ app/model/LDAPS/methodeLDAPS.php
@@ -33,4 +33,40 @@ function authentificationAuLDAP($username, $password){
         return false;
     }
 }
+
+/*
+ * Cette fonction permet de vérifier si un utilisateur est un administrateur
+ */
+function isAdmin($username) {
+    $ldap_conn = connectionLDAPS::getInstance()->getConnection();
+    if ($ldap_conn) {
+        // Se connecter d'abord avec un compte de service/administrateur
+        if (@ldap_bind($ldap_conn, "cn=admin,dc=mondomaine,dc=local", "adminpassword")) {
+            // Chercher le DN de l'utilisateur en utilisant l'attribut uid
+            $search = ldap_search($ldap_conn, "dc=mondomaine,dc=local", "(uid=" . $username . ")");
+            if ($search) {
+                $entries = ldap_get_entries($ldap_conn, $search);
+                if ($entries["count"] > 0) {
+                    $userdn = $entries[0]["dn"];
+                    // Vérifier si l'utilisateur est un membre du groupe "Administrateurs"
+                    $searchGroup = ldap_search($ldap_conn, "dc=mondomaine,dc=local", "(&(objectClass=groupOfNames)(member=" . $userdn . ")(cn=Administrateurs))");
+                    $groupEntries = ldap_get_entries($ldap_conn, $searchGroup);
+                    if ($groupEntries["count"] > 0) {
+                        return true; // L'utilisateur est un administrateur
+                    } else {
+                        return false; // L'utilisateur n'est pas un administrateur
+                    }
+                } else {
+                    return false; // L'utilisateur n'a pas été trouvé
+                }
+            } else {
+                return false; // La recherche LDAP a échoué
+            }
+        } else {
+            return false; // Connexion avec le compte admin échouée
+        }
+    } else {
+        return false; // Connexion LDAP échouée
+    }
+}
 ?>
