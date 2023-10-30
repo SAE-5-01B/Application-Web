bede1b326facd66dad78441bf86ab79d5e6c9f25
gaetanana <gaetan.gonfiantini@gmail.com>
2023-10-30 10:14:10 +0100

    Amélioration de l'authentification

--- app/controller/verificationAuthentification.php
+++ app/controller/verificationAuthentification.php
@@ -1,30 +1,17 @@
 <?php
-require "./../model/LDAPS/connectionLDAPS.php";
 
-// Connexion au LDAPS
-$ldap_conn = connectionLDAPS::getInstance()->getConnection();
 
-// Si on n'est pas connecté au LDAP, on ne poursuit pas
-if(!$ldap_conn){
-    header("Location: ./../view/pageAuthentificationEchoue.html");
-    exit;
-}
+
+require "./../model/LDAPS/methodeLDAPS.php";
 
 if(isset($_POST['username']) && isset($_POST['password'])){
-    // Authentification
     $username = $_POST['username'];
     $password = $_POST['password'];
-    if ($ldap_conn) {
-        $ldaprdn = "cn=" . $username . ",cn=Groupe G3,dc=mondomaine,dc=local";
-        // Connexion LDAP avec l'identifiant et le mot de passe de l'utilisateur
-        if (@ldap_bind($ldap_conn, $ldaprdn, $password)) {
-            $message = "Connexion LDAP réussie en tant que $username!";
-        } else {
-            $message = "Erreur d'authentification pour $username";
-        }
-    } else {
-        $message = "Connexion LDAP échouée...";
+    if(authentificationAuLDAP($username, $password)){
+        echo "Connexion réussie";
+    }else{
+        echo "Connexion échouée";
     }
-    echo $message;
+
 }
 ?>
--- app/model/LDAPS/connectionLDAPS.php
+++ app/model/LDAPS/connectionLDAPS.php
@@ -6,18 +6,11 @@ class connectionLDAPS{
 
     // Paramètres de la connexion LDAP pour Docker
     private $ldap_host_port = "ldap://ldap:389";
-    // Pour utiliser LDAPS (une fois mis en place), changez l'URL ci-dessus par:
-    // private $ldap_host_port = "ldaps://ldap:636";
+
 
     private function __construct() {
         $this->ldap_conn = ldap_connect($this->ldap_host_port);
-        // Assurez-vous d'avoir la version appropriée de LDAP
         ldap_set_option($this->ldap_conn, LDAP_OPT_PROTOCOL_VERSION, 3);
-
-        // Si vous passez à LDAPS, vous devriez aussi définir la vérification des certificats:
-        // ldap_set_option($this->ldap_conn, LDAP_OPT_X_TLS_REQUIRE_CERT, LDAP_OPT_X_TLS_NEVER);
-        // Notez que LDAP_OPT_X_TLS_NEVER est pour les environnements de test, il désactive la vérification des certificats.
-        // Pour un environnement de production, vous devriez probablement utiliser LDAP_OPT_X_TLS_HARD.
     }
     public static function getInstance() {
         if (self::$instance === null) {
