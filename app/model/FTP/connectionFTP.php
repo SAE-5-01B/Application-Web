9603936a96633223b85e1562f2ffcbfe0bd0c628
gaetanana <108981554+gaetanana@users.noreply.github.com>
2023-11-04 10:40:29 +0100

    Début du service FTP avec LDAP

--- app/model/FTP/connectionFTP.php
+++ app/model/FTP/connectionFTP.php
@@ -1,1 +1,38 @@
 <?php
+
+class connectionFTP {
+    private static $instance = null;
+
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+
+    private function __construct($host, $username, $password) {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->connect();
+    }
+    public static function getInstance($host = 'localhost', $username = 'user', $password = 'pass') {
+        if (self::$instance === null) {
+            self::$instance = new connectionFTP($host, $username, $password);
+        }
+        return self::$instance;
+    }
+    private function connect() {
+        $this->connection = ftp_connect($this->host) or die("Couldn't connect to $this->host");
+        if (!ftp_login($this->connection, $this->username, $this->password)) {
+            die("Couldn't login to $this->host as $this->username");
+        }
+    }
+    public function getConnection() {
+        return $this->connection;
+    }
+    public function close() {
+        if ($this->connection) {
+            ftp_close($this->connection);
+        }
+    }
+}
+?>
