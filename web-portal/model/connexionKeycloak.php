b31d03758ac29bf77e40cfbb1fe7d8af5d33bba9
gaetanana <gaetan.gonfiantini@gmail.com>
2023-11-21 16:17:22 +0100

    Keycloak php avance

--- web-portal/model/connexionKeycloak.php
+++ web-portal/model/connexionKeycloak.php
@@ -1,20 +1,27 @@
 <?php
+require_once './../vendor/autoload.php';
 use Jumbojett\OpenIDConnectClient;
-class connexionKeycloak {
+
+class ConnexionKeycloak {
     private static $instance = null;
     private $oidc;
 
     private function __construct() {
-        $this->oidc = new OpenIDConnectClient(
-            'http://keycloak-server:8080', // URL du serveur Keycloak
-            'aae06b07-e193-4bdd-9c18-bbff9597f84e',// ID client de Keycloak
-            'eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJmNWVjMDVlZS1mODA0LTQyMzUtYTBkNC0zNDdlYjAwMTI5ZGEifQ.eyJleHAiOjE3MzIwMjgwMjYsImlhdCI6MTcwMDQ5MjAyNiwianRpIjoiYWFlMDZiMDctZTE5My00YmRkLTljMTgtYmJmZjk1OTdmODRlIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL21hc3RlciIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC9hdXRoL3JlYWxtcy9tYXN0ZXIiLCJ0eXAiOiJJbml0aWFsQWNjZXNzVG9rZW4ifQ.ZHhRN-VT1NrmQpQrW-VS2odDAANgS4jwHR4PFYUS9w0'              // Clé secrète du client
-        );
+        try {
+            $this->oidc = new OpenIDConnectClient(
+                'http://keycloak-server:8080',
+                'aae06b07-e193-4bdd-9c18-bbff9597f84e',
+                'eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJmNWVjMDVlZS1mODA0LTQyMzUtYTBkNC0zNDdlYjAwMTI5ZGEifQ.eyJleHAiOjE3MzIwMjgwMjYsImlhdCI6MTcwMDQ5MjAyNiwianRpIjoiYWFlMDZiMDctZTE5My00YmRkLTljMTgtYmJmZjk1OTdmODRlIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL21hc3RlciIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC9hdXRoL3JlYWxtcy9tYXN0ZXIiLCJ0eXAiOiJJbml0aWFsQWNjZXNzVG9rZW4ifQ.ZHhRN-VT1NrmQpQrW-VS2odDAANgS4jwHR4PFYUS9w0'
+            );
+
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
     }
 
     public static function getInstance() {
         if (self::$instance === null) {
-            self::$instance = new connexionKeycloak();
+            self::$instance = new ConnexionKeycloak();
         }
         return self::$instance;
     }
@@ -23,4 +30,6 @@ class connexionKeycloak {
         return $this->oidc;
     }
 }
+
+
 ?>
