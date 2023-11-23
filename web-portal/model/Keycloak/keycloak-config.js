96de2efe9596536aa2e7661cc4614a810a87273c
gaetanana <gaetan.gonfiantini@gmail.com>
2023-11-23 14:12:58 +0100

    Changement config pour la nouvelle image keycloak

--- web-portal/model/Keycloak/keycloak-config.js
+++ web-portal/model/Keycloak/keycloak-config.js
@@ -1,5 +1,5 @@
 const keycloakConfig = {
-    url: 'http://localhost:8080/auth', // URL de votre serveur Keycloak
+    url: 'http://localhost:8080/', // URL de votre serveur Keycloak
     realm: 'CATS', // Le nom de votre realm
     clientId: 'b74fb1bc-6057-4234-bef6-17d9cd47d9b6', // L'ID de votre client Keycloak
 };
