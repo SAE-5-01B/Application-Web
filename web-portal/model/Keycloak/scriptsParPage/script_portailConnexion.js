49545074a43f00daa2c1b4bb5832445813ea3867
gaetanana <108981554+gaetanana@users.noreply.github.com>
2023-12-15 12:51:55 +0100

    On automatise :
    - Keycloak avec un royaume
    - LDAP avec un LDIF intrégré au lancement du compose

--- web-portal/model/Keycloak/scriptsParPage/script_portailConnexion.js
+++ web-portal/model/Keycloak/scriptsParPage/script_portailConnexion.js
@@ -3,8 +3,6 @@ window.addEventListener('load', () => {
     const logoutBtn = document.getElementById('logout');
 
     keycloak.init({ onLoad: 'check-sso' }).then(authenticated => {
-        console.log("Miaou");
-
         if (authenticated) {
             //Gestion de l'utilisateur authentifié
             window.location.replace("http://localhost:8080/realms/CATS/protocol/openid-connect/auth?client_id=portal-cats&redirect_uri=https%3A%2F%2Flocalhost%3A8443%2Fview%2FUtilisateurs%2FespacePersonnelUtilisateur.html&response_mode=fragment&response_type=code&scope=openid&nonce=e8f2162f-2d69-4ed5-b1fa-8931db645871");
