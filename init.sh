aebace30d8e542d3f62ec15e9d3c46fe8cacd589
gaetanana <gaetan.gonfiantini@gmail.com>
2024-01-09 12:08:04 +0100

    Modification conf

--- init.sh
+++ init.sh
@@ -3,14 +3,28 @@
 #####################################
 
 # Demande à l'utilisateur l'adresse IP du serveur pour Rocket.Chat
-read -p "Adresse IP du serveur Rocket.Chat : " ip
+read -p "Adresse IP du serveur Rocket.Chat (keycloak pour l'environnement de test sinon l'adresse IP du serveur) : " ipRocketChat
+echo "IP Rocket.Chat saisie : $ipRocketChat"
 
-# Vérification si ROCKET_CHAT_SERVER_IP est déjà défini dans .env
-if grep -q "ROCKET_CHAT_SERVER_IP=" .env; then
-    sed -i "s/ROCKET_CHAT_SERVER_IP=.*/ROCKET_CHAT_SERVER_IP=$ip/" .env
-else
-    echo "ROCKET_CHAT_SERVER_IP=$ip" >> .env
-fi
+# Demande à l'utilisateur l'adresse IP du serveur pour les autres services (Web, Nextcloud, RocketChat, Keycloak)
+read -p "Adresse IP du serveur pour les autres services (Web, Nextcloud, RocketChat, Keycloak) (soit localhost ou l'adresse IP du serveur) : " ipServeur
+echo "IP serveur saisie : $ipServeur"
+
+# Lecture et modification du fichier .env
+while IFS= read -r line; do
+    # Modification de ROCKET_CHAT_SERVER_IP
+    if [[ "$line" == "ROCKET_CHAT_SERVER_IP="* ]]; then
+        echo "ROCKET_CHAT_SERVER_IP=$ipRocketChat"
+    # Modification de SERVER_IP
+    elif [[ "$line" == "SERVER_IP="* ]]; then
+        echo "SERVER_IP=$ipServeur"
+    else
+        echo "$line"
+    fi
+done < .env > .env.tmp
+
+# Remplacement du fichier .env par le fichier temporaire modifié
+mv .env.tmp .env
 
 # Lancement du docker-compose
 docker-compose up -d
